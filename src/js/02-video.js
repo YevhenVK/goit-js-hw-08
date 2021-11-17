678d1a99dcefcf34b014f9d9389ebac6940f445a
YevhenVK <onegin797@gmail.com>
2021-11-17 06:29:48 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+import Player from '@vimeo/player';
+
+
+// const videoContainer = document.querySelector('#vimeo-player');
+// const videoFromGallery = createElementsFromGallery(Player);
+
+// videoContainer.insertAdjacentHTML('afterbegin', videoFromGallery);
+
+
+
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 740
+});
+
+
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
+player.on('play', onPlay);
+
+
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
 
