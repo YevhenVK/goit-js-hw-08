92922cd9a1f92a4d30ec0c58057058f3d0cd476b
YevhenVK <onegin797@gmail.com>
2021-11-21 08:04:56 +0200

    1

--- dist/01-gallery.82582570.js
+++ dist/01-gallery.82582570.js
@@ -2066,7 +2066,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58420" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55981" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -25,14 +25,13 @@ const onPlay = function(data) {
 player.on('play', onPlay);
 
 
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
+    // const iframe = document.querySelector('iframe');
+    // const player = new Vimeo.Player(iframe);
 
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
+    // player.on('play', function() {
+    //     console.log('played the video!');
+    // });
 
-// player.getVideoTitle().then(function(title) {
-//     console.log('title:', title);
-// });
-
+    // player.getVideoTitle().then(function(title) {
+    //     console.log('title:', title);
+    // });
